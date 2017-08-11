61f2f3a2274f5cd930a60eaedfb4ff4484224cb4
mars0i <mabrams@uab.edu>
2017-08-11 12:05:46 -0500

    switched to Clojure 1.9 alpha

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject popgen "0.1.0-SNAPSHOT"
   :description "misc population genetics experiments"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :license {:name "Gnu General Public License version 3.0"
+            :url "http://www.gnu.org/copyleft/gpl.html"}
+  :dependencies [;[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojure "1.9.0-alpha17"]
+                ])
