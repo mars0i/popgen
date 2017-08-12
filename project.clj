6366f39ede7b71df25b220d4676db0f88f242669
mars0i <mabrams@uab.edu>
2017-08-12 14:40:51 -0500

    added rm-hull/infix

--- project.clj
+++ project.clj
@@ -5,4 +5,6 @@
             :url "http://www.gnu.org/copyleft/gpl.html"}
   :dependencies [;[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojure "1.9.0-alpha17"]
+                 [rm-hull/infix "0.2.12"] ; https://github.com/rm-hull/infix
+                 ;; Note Incanter also has infix capability
                 ])
